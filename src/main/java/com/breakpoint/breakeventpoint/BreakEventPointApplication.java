77b5c2fdf5106cea108e35e7a7936d3f1bf51cd5
git\Enrique Bernardo González Vázquez <ebgvdeveloper@gmail.com>
2024-04-30 17:46:39 -0600

    Crear proyecto

---
+++ src/main/java/com/breakpoint/breakeventpoint/BreakEventPointApplication.java
@@ -0,0 +1,13 @@
+package com.breakpoint.breakeventpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreakEventPointApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BreakEventPointApplication.class, args);
+    }
+
+}
