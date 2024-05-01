3eaf8ad2bac6a5ef93b6d4f92733764ba72f5487
git\Enrique Bernardo González Vázquez <ebgvdeveloper@gmail.com>
2024-04-30 18:37:12 -0600

    Create CostoEntity

---
+++ src/main/java/com/breakpoint/breakeventpoint/models/entities/CostoEntity.java
@@ -0,0 +1,67 @@
+package com.breakpoint.breakeventpoint.models.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.Objects;
+
+@Entity
+public class CostoEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String descripcion;
+    private double monto;
+
+    public CostoEntity(){}
+
+    public CostoEntity(String descripcion, double monto) {
+        this.descripcion = descripcion;
+        this.monto = monto;
+    }
+
+    public CostoEntity(Long id, String descripcion, double monto) {
+        this.id = id;
+        this.descripcion = descripcion;
+        this.monto = monto;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getMonto() {
+        return monto;
+    }
+
+    public void setMonto(double monto) {
+        this.monto = monto;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CostoEntity that = (CostoEntity) o;
+        return Double.compare(that.monto, monto) == 0 && id.equals(that.id) && descripcion.equals(that.descripcion);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, descripcion, monto);
+    }
+}
